6e4f404789389088283a1388618dec6359de6d37
Nicolas Wirth <nicolas.wirth@speedgoat.ch>
2025-11-06 14:22:00 +0100

    Add new plugin: git-blame.lua
    Signed-off-by: Nicolas Wirth <nicolas.wirth@speedgoat.ch>

--- src/.config/nvim/lua/lazy/lazy-plugins.lua
+++ src/.config/nvim/lua/lazy/lazy-plugins.lua
@@ -7,28 +7,29 @@
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-  -- [[ Editor plugins ]]
-  require 'plugins.editor.autopairs',
-  require 'plugins.editor.conform',
-  require 'plugins.editor.colorscheme',
-  require 'plugins.editor.guess-indent',
-  require 'plugins.editor.indent_line',
-  require 'plugins.editor.lsp',
-  require 'plugins.editor.todo-comments',
-  require 'plugins.editor.treesitter',
+require("lazy").setup({
+	-- [[ Editor plugins ]]
+	require("plugins.editor.autopairs"),
+	require("plugins.editor.conform"),
+	require("plugins.editor.colorscheme"),
+	require("plugins.editor.guess-indent"),
+	require("plugins.editor.indent_line"),
+	require("plugins.editor.lsp"),
+	require("plugins.editor.todo-comments"),
+	require("plugins.editor.treesitter"),
 
-  -- [[ Language plugins]]
-  require 'plugins.lang.bitbake',
-  require 'plugins.lang.blink-cmp',
-  require 'plugins.lang.lint',
+	-- [[ Language plugins]]
+	require("plugins.lang.bitbake"),
+	require("plugins.lang.blink-cmp"),
+	require("plugins.lang.lint"),
 
-  -- [[ Utils plugins]]
-  require 'plugins.utils.gitsigns',
-  require 'plugins.utils.mini',
-  require 'plugins.utils.neo-tree',
-  require 'plugins.utils.telescope',
-  require 'plugins.utils.which-key',
+	-- [[ Utils plugins]]
+	require("plugins.utils.git-blame"),
+	require("plugins.utils.gitsigns"),
+	require("plugins.utils.mini"),
+	require("plugins.utils.neo-tree"),
+	require("plugins.utils.telescope"),
+	require("plugins.utils.which-key"),
 })
 
 -- vim: ts=2 sts=2 sw=2 et
