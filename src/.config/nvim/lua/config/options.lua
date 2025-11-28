7f7bc22bdbed0e722709b37447d9f566357e7d2f
Nicolas Wirth <nicolas.wirth@speedgoat.ch>
2025-11-28 08:55:55 +0100

    Improve gitconfig and neovim
    Signed-off-by: Nicolas Wirth <nicolas.wirth@speedgoat.ch>

--- src/.config/nvim/lua/config/options.lua
+++ src/.config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0 -- set to 0 to default to tabstop value
+vim.opt.textwidth = 80
 
 -- Enable line number
 vim.opt.number = true
@@ -22,7 +23,7 @@ vim.opt.splitright = true
 vim.g.have_nerd_font = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -32,7 +33,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -49,10 +50,10 @@ vim.o.timeoutlen = 300
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.o.cursorline = true
